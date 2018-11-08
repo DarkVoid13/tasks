956baf510a394024ed01ee011024ac73c2698a81
DarkVoid13 <44837686+DarkVoid13@users.noreply.github.com>
2018-11-08 03:01:47 +0300

    Update task1.go

--- task1.go
+++ task1.go
@@ -5,26 +5,26 @@ import (
 	"fmt"
 )
 
-type animals struct {
+type Animals struct {
 	age    int
 	weight int
 	color  string
 }
 type Duck struct {
-	animals
+	Animals
 	name   string
 	height int
 	price  float32
 }
 
-func (a animals) Inf1() (string, int, int) {
+func (a Animals) Inf1() (string, int, int) {
 	return a.color, a.age, a.weight
 }
-func Inf2(a animals) (int, int) {
+func Inf2(a Animals) (int, int) {
 	return a.age, a.weight
 }
 func main() {
-	a1 := animals{12, 14, "blue"}
+	a1 := Animals{12, 14, "blue"}
 	fmt.Println(a1.Inf1())
 	fmt.Println(Inf2(a1))
 }
